ece5162a59eebd8f347fc01ad4485d31eb1f02f8
Nasser Ghazali <nghazali@gmail.com>
2022-11-11 14:10:36 +1100

    Commented confluence provisioning temporarily

--- modules/AZURE/postgres/locals.tf
+++ modules/AZURE/postgres/locals.tf
@@ -2,7 +2,7 @@ locals {
   zone                  = "1"
   backup_retention_days = 7
   instance_name         = "${var.vnet_prefix}-${var.product}-server"
-  network_security_name = "${var.vnet_prefix}-nsg"
-  security_rule_name    = "${var.vnet_prefix}-rds"
+  network_security_name = "${var.vnet_prefix}-rds-${var.product}-nsg"
+  security_rule_name    = "${var.vnet_prefix}-${var.product}-rds"
   database_name         = var.product
 }
